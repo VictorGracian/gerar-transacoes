866a55ad941faf972498b2e077024515e558ffc4
Victor Enrique Cavalheri Graciano <victor.graciano@duartecon.local>
2024-03-18 14:01:07 -0300

    execucao

--- gerar_transacoes/__init__.py
+++ gerar_transacoes/__init__.py
@@ -1,9 +1,11 @@
 from gerar_transacoes.gerador_de_transacoes import abrirSistema
-from gerar_transacoes.gerador_de_transacoes import gerarTransacoes
+from gerar_transacoes.gerador_de_transacoes import selecionarEmpresa
+from gerar_transacoes.gerador_de_transacoes import gerarTransacao
 import time
 
 
 abrirSistema()
 time.sleep(10)
-gerarTransacoes()
-
+selecionarEmpresa()
+time.sleep(2)
+gerarTransacao()
--- gerar_transacoes/gerador_de_transacoes.py
+++ gerar_transacoes/gerador_de_transacoes.py
@@ -25,21 +25,52 @@ def abrirSistema():
     robo.click(x=462, y=446) # clicando em entrar
     print("sistema aberto")
 
-def gerarTransacoes():
+def selecionarEmpresa():
+    #Selecionando empresa
     robo.write("16")
     robo.press("enter")
     time.sleep(1)
+    
+    #Selecionando filial
     robo.write("1")
     robo.press("enter")
     time.sleep(1)
+    
+    #Selecionando periodo
     robo.write("01/01/2020")
     robo.press("enter")
     time.sleep(1)
     robo.write("31/12/2100")
+    
+    #Finalizando
     robo.press("enter")
     robo.press("enter")
     print("empresa selecionada")
-
-
+    
+def gerarTransacao():
+    #Escolhendo a rotina
+    robo.click(x=378, y=31)
+    time.sleep(1)
+    robo.click(x=477, y=409)
+    time.sleep(1)
+    robo.click(x=806, y=419)
+    time.sleep(1)
+    
+    #Configuração
+    robo.doubleClick(x=287, y=134)
+    time.sleep(1)
+    robo.write("01/01/2020")
+    time.sleep(1)
+    robo.press("enter")
+    time.sleep(1)
+    robo.write("31/12/2100")
+    time.sleep(1)
+    
+    #execução
+    robo.click(x=305, y=99)
+    time.sleep(1)
+    robo.click(x=890, y=403)
+    
+    
 time.sleep(3)
 robo.position()
